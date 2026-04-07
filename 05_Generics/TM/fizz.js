f320bb4658d3fd8fe77cf2c76afa349c5a0978cd
ZrecKun <bintangsurya008@gmail.com>
2026-04-07 23:45:55 +0700

    TM 5

---
+++ 05_Generics/TM/fizz.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ * * @param {number} value 
+ * @returns {string | number}
+ * @throws {Error}
+ */
+function zzzzOrNum(value) {
+    if (typeof value !== 'number' || isNaN(value)) {
+        throw new Error("Input harus berupa angka tunggal.");
+    }
+
+    if (value % 3 === 0 && value % 5 === 0) {
+        return "FizzBuzz";
+    } else if (value % 3 === 0) {
+        return "Fizz";
+    } else if (value % 5 === 0) {
+        return "Buzz";
+    } else {
+        return value;
+    }
+}
+
+/**
+ * * @param {number[]} sequence
+ * @returns {(string | number)[]}
+ * @throws {Error}
+ */
+function fizzBuzz(sequence) {
+    if (!Array.isArray(sequence)) {
+        throw new Error("Input harus berupa array.");
+    }
+
+    const newSequence = sequence.map((e) => zzzzOrNum(e));
+
+    return newSequence;
+}
+
+module.exports = {
+    fizzBuzz,
+    zzzzOrNum,
+};
