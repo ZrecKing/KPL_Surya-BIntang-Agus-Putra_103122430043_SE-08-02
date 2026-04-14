0292412c02c498050f9e739b01b1a276988f85d8
ZrecKun <bintangsurya008@gmail.com>
2026-04-14 23:20:16 +0700

    TM 06

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/GUIDED/Guided.js
@@ -0,0 +1,21 @@
+// Fungsi untuk melakukan pembagian
+function div(a, b) {
+    if (typeof a !== 'number' && typeof b !== 'number') {
+        throw new TypeError("Salah tipe data untuk argumen a");
+    }
+
+    if (b === 0) {
+        return 0;
+    }
+
+    const hasil = a / b;
+
+    // Pascakondisi
+    if (hasil * b === a) {
+        return hasil;
+    }
+
+    return 0;
+}
+
+console.log(div(20, 40));
